9a75393700caad248635528b692655611e7e92da
Wekoslav Stefanovski <swekster@gmail.com>
2017-12-01 12:33:45 +0100

    2016 day 09

--- 2016/code/day-09.ts
+++ 2016/code/day-09.ts
@@ -1,4 +1,5 @@
 import { readInput, readInputLines } from "../extra/aoc-helper";
+import { isNumber } from "util";
 
 async function main() {
     let line = await readInput();
@@ -32,8 +33,49 @@ function processPartOne(compress: string) {
 
 function processPartTwo(compress: string) {
     let totalLength = 0;
-    const match = compress.match(/^\((\d+)x(\d+)\)/);
-    
+    const tokens = tokenize(compress);
+    return sumTokens(tokens);
+}
+
+function sumTokens(tokens){
+    let sum = 0;
+    for (let index = 0; index < tokens.length; index++) {
+        const element = tokens[index];
+        if (isNumber(element)){
+            sum +=element;
+        } else {
+            sum += element.times * sumTokens(element.tokens);
+        }
+    }
+    return sum;
+}
+
+function tokenize(input: string) {
+    const tokens = []
+
+    while (input) {
+        const start = input.indexOf("(");
+        if (start === -1) {
+            tokens.push(input.length);
+            return tokens;
+        }
+        if (start !== 0) {
+            tokens.push(start);
+            input = input.substr(start);
+        }
+
+        const match = input.match(/^\((\d+)x(\d+)\)/);
+
+        const len = parseInt(match[1]);
+        const times = parseInt(match[2]);
+        const token = input.substr(match[0].length, len);
+        input = input.substr(match[0].length + len);
+        tokens.push({
+            tokens: tokenize(token),
+            times: times
+        });
+    }
+    return tokens;
 }
 
 main();
