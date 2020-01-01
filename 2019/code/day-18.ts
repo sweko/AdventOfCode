e89e15f580ce4936cd9d877b58d5cb9cf4e85bd0
Wekoslav Stefanovski <swekster@gmail.com>
2020-01-01 14:12:37 +0100

    Day 18.2019 part 1

--- 2019/code/day-18.ts
+++ 2019/code/day-18.ts
@@ -1,6 +1,7 @@
 import { readInputLines, readInput } from "../extra/aoc-helper";
 import "../extra/array-helpers";
 import { Puzzle } from "./model";
+import { printMatrix } from "../extra/terminal-helper";
 
 interface Point {
     x: number,
@@ -23,9 +24,12 @@ interface Maze {
     keys: Point[];
     doors: Point[];
     robot: Point;
+    path: string[];
+    pathstr?: string;
+    distance: number;
 }
 
-const processInput = async (day: number) => {
+const processInput = async (day: number): Promise<Maze> => {
     const lines = await readInputLines(day);
     let robot: Point;
     const doors: Point[] = [];
@@ -35,7 +39,7 @@ const processInput = async (day: number) => {
         if (char === ".") return { x, y, state: State.Empty };
         if (char === "@") {
             robot = { x, y, state: State.Robot, distance: 0 }
-            return { x, y, state: State.Robot, distance: 0 };
+            return { x, y, state: State.Robot, distance: 0, name: "@" };
         }
         if (char === char.toLowerCase()) {
             keys.push({ x, y, state: State.Key, name: char })
@@ -46,18 +50,18 @@ const processInput = async (day: number) => {
     }));
     keys.sort((f, s) => f.name.localeCompare(s.name));
     doors.sort((f, s) => f.name.localeCompare(s.name));
-    return { cells, keys, doors, robot }
+    return { cells, keys, doors, robot, path: [], distance: 0 }
 };
 
 const checkNeighbour = (point: Point) => {
     if (point.state === State.Empty && point.distance === undefined) return { reachable: true };
     if (point.state === State.Key && point.distance === undefined) return { reachable: true, key: point.name };
-    return {reachable: false};
+    return { reachable: false };
 }
 
 const floodFill = (maze: Maze) => {
     const active = [maze.robot];
-    const distances = [];
+    const distances: Point[] = [];
     while (active.length > 0) {
         const current = active.shift();
 
@@ -65,50 +69,166 @@ const floodFill = (maze: Maze) => {
         const up = checkNeighbour(neighbour);
         if (up.reachable) {
             neighbour.distance = current.distance + 1;
-            active.push(neighbour)
             if (up.key) {
                 distances.push(neighbour);
+            } else {
+                active.push(neighbour)
             }
         }
-        
+
         neighbour = maze.cells[current.x + 1][current.y];
         const down = checkNeighbour(neighbour);
         if (down.reachable) {
             neighbour.distance = current.distance + 1;
-            active.push(neighbour)
             if (down.key) {
                 distances.push(neighbour);
+            } else {
+                active.push(neighbour)
             }
         }
 
-        neighbour = maze.cells[current.x][current.y-1];
+        neighbour = maze.cells[current.x][current.y - 1];
         const left = checkNeighbour(neighbour);
         if (left.reachable) {
             neighbour.distance = current.distance + 1;
-            active.push(neighbour)
             if (left.key) {
                 distances.push(neighbour);
+            } else {
+                active.push(neighbour)
             }
         }
 
-        neighbour = maze.cells[current.x][current.y+1];
+        neighbour = maze.cells[current.x][current.y + 1];
         const right = checkNeighbour(neighbour);
         if (right.reachable) {
             neighbour.distance = current.distance + 1;
-            active.push(neighbour)
             if (right.key) {
                 distances.push(neighbour);
+            } else {
+                active.push(neighbour)
             }
         }
     }
     return distances;
 }
 
-const partOne = (maze: Maze, debug: boolean) => {
-    const reachable = floodFill(maze);
-    console.log(reachable);
+const distanceCache = {};
+const fullCache = {};
 
-    return 0;
+const moveRobot = (maze: Maze, destination: Point) => {
+
+    const spath = destination.name.toUpperCase() + maze.path.slice().sort().join("");
+    const cacheValue = fullCache[spath];
+
+    if (cacheValue && cacheValue < destination.distance) {
+        // console.log(`Found cache value for ${maze.pathstr}, i.e. ${spath} with value ${cacheValue}`);
+        // console.log(`  Current distance is ${destination.distance}`);
+
+        return undefined;
+    }
+
+    fullCache[spath] = destination.distance;
+
+    if (destination.state !== State.Key) {
+        throw Error("Destination is not a key")
+    }
+    const result: Maze = {
+        distance: maze.distance,
+        path: maze.path.slice(),
+        doors: maze.doors.map(d => ({ x: d.x, y: d.y, name: d.name, state: State.Door })),
+        keys: maze.keys.map(k => ({ x: k.x, y: k.y, name: k.name, state: State.Key })),
+        cells: maze.cells.map(row => row.map(p => (({ x: p.x, y: p.y, name: p.name, state: p.state })))),
+        robot: undefined
+    }
+    // set the distance
+    result.distance = destination.distance;
+    // add the destination to the path
+    result.path.push(destination.name);
+    result.pathstr = result.path.join("");
+
+    // remove the taken key from the available keys
+    result.keys = [...result.keys.filter(k => k.name !== destination.name)];
+
+    // move the robot to the key position
+    result.robot = {
+        state: State.Robot,
+        x: destination.x,
+        y: destination.y,
+        distance: result.distance,
+        name: "@"
+    };
+    result.cells[destination.x][destination.y] = result.robot;
+
+    // remove the old robot
+    result.cells[maze.robot.x][maze.robot.y] = {
+        x: maze.robot.x,
+        y: maze.robot.y,
+        state: State.Empty
+    };
+
+    // remove the door from the maze and the doors collection
+    const door = result.doors.find(d => d.name === destination.name.toUpperCase());
+    // console.log("Door is", door);
+    result.doors = [...result.doors.filter(d => d !== door)];
+    if (door) {
+        result.cells[door.x][door.y] = {
+            x: door.x,
+            y: door.y,
+            state: State.Empty
+        };
+    }
+
+    return result;
+}
+
+const partOne = (input: Maze, debug: boolean) => {
+    let mazes = [input];
+
+    let minPath = Number.POSITIVE_INFINITY;
+
+    let runs = 0;
+
+    while (mazes.length) {
+        runs += 1;
+        const maze = mazes.shift();
+        // printMatrix(maze.cells, point => point.name || (point.state === State.Empty ? "." : "#"));
+        // console.log(maze.distance);
+
+        let reachable = floodFill(maze);
+        // console.log(reachable);
+
+        const descendants = reachable.map(key => moveRobot(maze, key))
+            .filter(m => m)
+            .filter(m => m.distance < minPath);
+        const finished = descendants.filter(m => m.keys.length === 0);
+        const unfinished = descendants.filter(m => m.keys.length !== 0);
+        for (const m of finished) {
+            distanceCache[m.pathstr] = m.distance;
+        }
+        minPath = Object.values(distanceCache).min();
+        for (const maze of finished) {
+            console.log(maze.pathstr, maze.distance, minPath, "\u001b[K");
+        }
+        if (runs % 1237 === 123) {
+            console.log("Current run: ", runs, " current path: ", maze.pathstr, "\u001b[K");
+            console.log("\u001b[2A");
+        }
+
+        mazes.push(...unfinished);
+        mazes.sort((f, s) => f.pathstr.localeCompare(s.pathstr));
+        // if (mazes.length % 1234 === 123) {
+        //     console.log(mazes.length);
+        //     console.log(mazes[0].path.join());
+        // }
+
+        // console.log("--------------");
+    }
+
+    // for (const maze of distances) {
+    //     console.log(maze.path.join(""), maze.distance);
+    // }
+    console.log("Total runs:", runs)
+    return minPath;
 };
 
 const partTwo = (input: Maze, debug: boolean) => {
@@ -120,7 +240,7 @@ const partTwo = (input: Maze, debug: boolean) => {
 };
 
 const resultOne = (_: any, result: number) => {
-    return `Total system energy is ${result}`;
+    return `Minimal steps to collect all keys are ${result}`;
 };
 
 const resultTwo = (_: any, result: number) => {
