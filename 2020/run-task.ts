164952e0ae6278ae5f7f1a80f46db777ef3bea32
Wekoslav Stefanovski <swekster@gmail.com>
2020-12-23 11:32:26 +0100

    Prep work AoC 2020

--- 2020/run-task.ts
+++ 2020/run-task.ts
@@ -17,6 +17,12 @@ import { solutionSixteen } from "./code/day-16";
 import { solutionSeventeen } from "./code/day-17";
 import { solutionEighteen } from "./code/day-18";
 import { solutionNineteen } from "./code/day-19";
+import { solutionTwenty } from "./code/day-20";
+import { solutionTwentyOne } from "./code/day-21";
+import { solutionTwentyTwo } from "./code/day-22";
+import { solutionTwentyThree } from "./code/day-23";
+import { solutionTwentyFour } from "./code/day-24";
+import { solutionTwentyFive } from "./code/day-25";
 
 
 import { performance } from "perf_hooks";
