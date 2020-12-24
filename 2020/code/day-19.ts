aa500f6aa5a3721e2add0951718a4d5bf3cdf650
Wekoslav Stefanovski <swekster@gmail.com>
2020-12-24 01:12:26 +0100

    AoC 2020, day 19 success

--- 2020/code/day-19.ts
+++ 2020/code/day-19.ts
@@ -7,6 +7,7 @@ interface TerminalRule {
   kind: 'terminal',
   index: number;
   terminal: string;
+  regex: string;
 }
 
 interface SubstitutionRule {
@@ -14,6 +15,7 @@ interface SubstitutionRule {
   index: number;
   first: [number, number?];
   second?: [number, number?];
+  regex: string;
 }
 
 type Rule = TerminalRule | SubstitutionRule;
@@ -36,7 +38,8 @@ const processInput = async (day: number) => {
       return {
         kind: 'terminal',
         index: +match[1],
-        terminal: match[2]
+        terminal: match[2],
+        regex: match[2],
       }
     }
     const result: SubstitutionRule = {
@@ -45,7 +48,8 @@ const processInput = async (day: number) => {
       first: [
         +match[3],
         match[4] ? +match[4] : null
-      ]
+      ],
+      regex: null
     };
     if (match[5]) {
       result.second = [
@@ -79,14 +83,69 @@ const isRuleValid = (id: number, value: string, rules: RuleHash) : boolean => {
   
 }
 
+const getRegex = (rule: SubstitutionRule, rules: RuleHash): string => {
+  const firstOne = rules[rule.first[0]].regex;
+  if (firstOne === null) {
+    return null;
+  }
+  const firstTwo = rules[rule.first[1]]?.regex;
+  if (firstTwo === null) {
+    return null;
+  }
+
+  const secondOne = rules[rule.second ? rule.second[0]: undefined]?.regex;
+  if (secondOne === null) {
+    return null;
+  }
+  const secondTwo = rules[rule.second ? rule.second[1]: undefined]?.regex;
+  if (secondTwo === null) {
+    return null;
+  }
+
+  const fregex = firstTwo ? `${firstOne}${firstTwo}` : firstOne;
+  const sregex = secondOne ? (secondTwo ? `${secondOne}${secondTwo}` : secondOne) : "";
+  const regex = sregex ? `(${fregex}|${sregex})` : fregex;
+  console.log(`Setting regex for rule #${rule.index} to ${regex}`);
+  return regex;
+}
+
 const partOne = ({rules, tests}: Input, debug: boolean) => {
   const ruleArray: Rule[] = Object.keys(rules).map(key => rules[key]);
 
-  return 0;
+  let rest = ruleArray.filter(rule => !rule.regex) as SubstitutionRule[];
+  while (rest.length) {
+    for (const rule of rest) {
+      rule.regex = getRegex(rule, rules);
+    }
+    rest = ruleArray.filter(rule => !rule.regex) as SubstitutionRule[];
+  }
+
+  const zero = rules[0];
+  const regex = new RegExp(`^${zero.regex}$`);
+
+  const result = tests.filter(test => regex.test(test)).length;
+
+  return result;
 };
 
-const partTwo = (input: Input, debug: boolean) => {
-  return 0;
+const partTwo = ({rules, tests}: Input, debug: boolean) => {
+
+  const ruleArray: Rule[] = Object.keys(rules).map(key => rules[key]);
+  // assume all the rules got their regex at part I
+  rules[8].regex = `(${rules[8].regex})+`
+
+  const eleven = rules[11] as SubstitutionRule;
+  const one = rules[eleven.first[0]].regex;
+  const two = rules[eleven.first[1]].regex;
+  eleven.regex = `((${one}${two})|(${one}{2}${two}{2})|(${one}{3}${two}{3})|(${one}{4}${two}{4})|(${one}{5}${two}{5})|(${one}{6}${two}{6}))`;
+
+  const zero = rules[0] as SubstitutionRule;
+  zero.regex = getRegex(zero, rules);
+  const regex = new RegExp(`^${zero.regex}$`);
+
+  const result = tests.filter(test => regex.test(test)).length;
+
+  return result;
 };
 
 const result = (_: any, result: number) => {
