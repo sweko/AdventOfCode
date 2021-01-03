7eb24af9eda35c21996a3c85a55c3ebfa33e4188
Wekoslav Stefanovski <swekster@gmail.com>
2021-01-03 20:04:29 +0100

    generic classes

--- 2020/code/day-20.ts
+++ 2020/code/day-20.ts
@@ -11,6 +11,12 @@ interface Tile<T=boolean> {
     values: T[][];
 }
 
+interface TileMetadata<T=boolean> {
+    id: number;
+    numbers: TileNumbers;
+    states: TileStates<T>
+}
+
 interface LineNumbers {
     straight: number;
     reverse: number;
@@ -29,6 +35,7 @@ interface TileState<T=boolean> {
 }
 
 interface TileStates<T> {
+    id: number;
     top: TileState<T>;
     bottom: TileState<T>;
     left: TileState<T>;
@@ -72,19 +79,19 @@ const getTileNumbers = ({values}: Tile): TileNumbers => {
     return {
         top: {
             straight: asNumber(topline),
-            reverse: asNumber(topline.reverse())
+            reverse: asNumber(topline.slice().reverse())
         },
         bottom: {
             straight: asNumber(bottomline),
-            reverse: asNumber(bottomline.reverse())
+            reverse: asNumber(bottomline.slice().reverse())
         },
         left: {
             straight: asNumber(leftline),
-            reverse: asNumber(leftline.reverse())
+            reverse: asNumber(leftline.slice().reverse())
         },
         right: {
             straight: asNumber(rightline),
-            reverse: asNumber(rightline.reverse())
+            reverse: asNumber(rightline.slice().reverse())
         }
     }
 }
@@ -121,56 +128,52 @@ const partOne = (input: Tile[], debug: boolean) => {
     return corners[0] * corners[1] * corners[2] * corners[3];
 };
 
-const emptyTile = <T>(size:number, dfault: T): Tile<T> => ({
-    id: 0,
-    values: new Array(size).fill(null).map(_ => new Array(size).fill(dfault))
-});
+const emptyTile = <T>(size:number, dfault: T): T[][] => new Array(size).fill(null).map(_ => new Array(size).fill(dfault));
 
-const getTileStates = <T>({id, values}:Tile<T>, dfault: T): Tile<T>[] => {//TileStates<T> => {
+const getTileStates = <T>({id, values}:Tile<T>, dfault: T): TileStates<T> => {
     const size = values.length;
 
-    const result = new Array(8).fill(null).map(_ => emptyTile(size, dfault));
-
-    // const topStraight = emptyTile(size, dfault);
-    // const topReverse = emptyTile(size, dfault);
-    // const bottomStraight = emptyTile(size, dfault);
-    // const bottomReverse = emptyTile(size, dfault);
-    // const leftStraight = emptyTile(size, dfault);
-    // const leftReverse = emptyTile(size, dfault);
-    // const rightStraight = emptyTile(size, dfault);
-    // const rightReverse = emptyTile(size, dfault);
+    const topStraight = emptyTile(size, dfault);
+    const topReverse = emptyTile(size, dfault);
+    const bottomStraight = emptyTile(size, dfault);
+    const bottomReverse = emptyTile(size, dfault);
+    const leftStraight = emptyTile(size, dfault);
+    const leftReverse = emptyTile(size, dfault);
+    const rightStraight = emptyTile(size, dfault);
+    const rightReverse = emptyTile(size, dfault);
     for (let row = 0; row < size; row += 1) {
         for (let column = 0; column < size; column +=1) {
-            result[0].values[row][column] = values[row][column];
-            result[1].values[row][column] = values[row][size-1-column];
-            result[2].values[row][column] = values[size-1-row][size-1-column];
-            result[3].values[row][column] = values[size-1-row][column];
-            result[4].values[row][column] = values[column][row];
-            result[5].values[row][column] = values[size-1-column][row];
-            result[6].values[row][column] = values[column][size-1-row];
-            result[7].values[row][column] = values[size-1-column][size-1-row];
+            topStraight[row][column] = values[row][column];
+            topReverse[row][column] = values[row][size-1-column];
+            bottomStraight[row][column] = values[size-1-row][column];
+            bottomReverse[row][column] = values[size-1-row][size-1-column];
+
+            leftStraight[row][column] = values[column][row];
+            leftReverse[row][column] = values[size-1-column][row];
+            rightStraight[row][column] = values[column][size-1-row];
+            rightReverse[row][column] = values[size-1-column][size-1-row];
         }
     }
-    return result;
 
-    // return {
-    //     top: {
-    //         straight: topStraight.values,
-    //         reverse: topReverse.values,
-    //     },
-    //     bottom: {
-    //         straight: bottomStraight.values,
-    //         reverse: bottomReverse.values,
-    //     },
-    //     left: {
-    //         straight: leftStraight.values,
-    //         reverse: leftReverse.values,
-    //     },
-    //     right: {
-    //         straight: rightStraight.values,
-    //         reverse: rightReverse.values
-    //     }
-    // };
+    return {
+        id,
+        top: {
+            straight: topStraight,
+            reverse: topReverse,
+        },
+        bottom: {
+            straight: bottomStraight,
+            reverse: bottomReverse,
+        },
+        left: {
+            straight: leftStraight,
+            reverse: leftReverse,
+        },
+        right: {
+            straight: rightStraight,
+            reverse: rightReverse
+        }
+    };
 
     // return [
     //     {
@@ -224,6 +227,13 @@ const getTileStates = <T>({id, values}:Tile<T>, dfault: T): Tile<T>[] => {//Tile
     // ];
 }
 
+const getTilesMetadata = <T>(input: Tile<T>[]):TileMetadata<T> => {
+    const tiles = input.map(tile => ({
+        id: tile.id,
+        numbers: getTileNumbers(tile),
+        states: getTileStates(tile, false);
+    }));
+}
 
 const getCorners = (input: Tile[]):[Tile, Tile, Tile, Tile] => {
     const tiles = input.map(tile => ({
@@ -231,39 +241,40 @@ const getCorners = (input: Tile[]):[Tile, Tile, Tile, Tile] => {
         numbers: getTileNumbers(tile)
     }));
 
-    const corners = tiles.flatMap(tile => [
+    const x0 = tiles.flatMap(tile => [
         { id: tile.id, value: Math.min(tile.numbers.top.straight, tile.numbers.top.reverse) },
         { id: tile.id, value: Math.min(tile.numbers.bottom.straight, tile.numbers.bottom.reverse) },
         { id: tile.id, value: Math.min(tile.numbers.left.straight, tile.numbers.left.reverse) },
         { id: tile.id, value: Math.min(tile.numbers.right.straight, tile.numbers.right.reverse) },
-    ])
-        .groupBy(item => item.value)
-        .filter(group => group.items.length % 2 !== 0)
-        .map(group => group.items)
-        .flat()
-        .groupBy(item => item.id)
-        .filter(group => group.items.length >= 2)
-        .map(group => group.key);
+    ]);
 
-    if (corners.length !== 4) {
-        console.log(corners);
+    const x1 = x0.groupBy(item => item.value);
+    const x2 = x1.filter(group => group.items.length % 2 !== 0);
+    const x3 = x2.map(group => group.items).flat();
+    const x4 = x3.groupBy(item => item.id);
+    const x5 = x4.filter(group => group.items.length >= 2);
+    const x6 = x5.map(group => group.key);
+
+    if (x6.length !== 4) {
+        console.log(x0);
         throw new Error("FOUR CORNERS NOT FOUND");
     }
-    return corners as any;
+    return x6 as any;
 }
 
-
-
 const partTwo = (input: Tile[], debug: boolean) => {
+    showInput(input);
     const imageSide = Math.sqrt(input.length);
     const tileSize = input[0].values.length;
+
+    const corners = getCorners(input);
 
     // const tiles = input.map(tile => ({
     //     id: tile.id,
     //     states: getTileStates(getTileNumbers(tile))
     // }));
 
-    // const corners = getCornerIds(input);
+    
     
     // const image: Tile[][] = new Array(imageSide).fill(null)
     //     .map(_ => new Array(imageSide).fill(null).map(_ => emptyTile(tileSize)));
@@ -297,11 +308,32 @@ const test = (_: Tile[]) => {
         ]
     };
 
-    for (const state of getTileStates(tile, "")) {
-        printMatrix(state.values);
-        console.log("----");
-        
-    }
+    const states = getTileStates(tile, "");
+    printMatrix(states.top.straight);
+    console.log("--");
+    printMatrix(states.top.reverse);
+    console.log("--");
+    printMatrix(states.bottom.straight);
+    console.log("--");
+    printMatrix(states.bottom.reverse);
+    console.log("--states.left.straight--");
+    printMatrix(states.left.straight);
+    console.log("--states.left.reverse--");
+    printMatrix(states.left.reverse);
+    console.log("--states.right.straight--");
+    printMatrix(states.right.straight);
+    console.log("--states.right.reverse--");
+    printMatrix(states.right.reverse);
+    console.log("--");
+
+    return;
+    printMatrix(states.top.straight);
+    printMatrix(states.top.straight);
+    printMatrix(states.top.straight);
+    printMatrix(states.top.straight);
+    printMatrix(states.top.straight);
+    printMatrix(states.top.straight);
+
     // console.log(JSON.stringify(, null, 2));
 };
 
