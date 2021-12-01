1afbb5ec8f1b4dd13e13fe1581bd2d07c69645d3
Wekoslav Stefanovski <swekster@gmail.com>
2021-12-01 11:52:48 +0100

    2021 - day 01

--- 2021/code/day-01.ts
+++ 2021/code/day-01.ts
@@ -3,7 +3,21 @@ import "../extra/array-helpers";
 import { Puzzle } from "./model";
 
 const processInput = async (day: number) => {
-    return [];
+    const input = await readInputLines(day);
+    return input.map(line => parseInt(line, 10));
+
+    // return [
+    //     199,
+    //     200,
+    //     208,
+    //     210,
+    //     200,
+    //     207,
+    //     240,
+    //     269,
+    //     260,
+    //     263,
+    // ];
 };
 
 const partOne = (input: number[], debug: boolean) => {
@@ -11,7 +25,18 @@ const partOne = (input: number[], debug: boolean) => {
         console.log("-------Debug-----");
     }
 
-    return 0;
+    let count = 0;
+
+    for (let i = 1; i < input.length; i++) {
+        const current = input[i];
+        const previous = input[i - 1];
+
+        if (current > previous) {
+            count += 1;
+        }
+    }
+
+    return count;
 };
 
 const partTwo = (input: number[], debug: boolean) => {
@@ -19,7 +44,24 @@ const partTwo = (input: number[], debug: boolean) => {
         console.log("-------Debug-----");
     }
 
-    return 0;
+    // calculate the windows
+    const windows = [];
+    for (let i = 2; i < input.length; i++) {
+        const window = input[i] + input[i - 1] + input[i - 2];
+        windows.push(window);
+    }
+
+    let count = 0;
+    for (let i = 1; i < windows.length; i++) {
+        const current = windows[i];
+        const previous = windows[i - 1];
+
+        if (current > previous) {
+            count += 1;
+        }
+    }
+
+    return count;
 };
 
 const resultOne = (_: any, result: number) => {
