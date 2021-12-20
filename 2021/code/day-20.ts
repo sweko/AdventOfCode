f3d36fe82e41ceda96e745fd566b75956ab31bd6
Wekoslav Stefanovski <swekster@gmail.com>
2021-12-20 22:50:51 +0100

    2021 - Day 20 - straight expand

--- 2021/code/day-20.ts
+++ 2021/code/day-20.ts
@@ -1,25 +1,117 @@
 import { readInputLines, readInput } from "../extra/aoc-helper";
 import "../extra/array-helpers";
+import { printMatrix } from "../extra/terminal-helper";
 import { Puzzle } from "./model";
 
+type Pixel = "."|"#";
+type Map  = Pixel[];
+type Image = Pixel[][];
+interface Input {
+    map: Map;
+    image: Image;
+}
+
 const processInput = async (day: number) => {
-    return [];
+    const lines = await readInputLines(day);
+    const map = lines[0].split("");
+    const image = lines.slice(2).map(line => line.split(""));
+    return {map, image} as Input;
 };
 
-const partOne = (input: number[], debug: boolean) => {
+const stepImage = (map: Map, image: Image, step: number): Image => {
+    const infinity = getInfityStone(map, step);
+
+    const expanded = [
+        Array(image[0].length + 4).fill(infinity),
+        Array(image[0].length + 4).fill(infinity),
+        ...image.map(line => [infinity, infinity, ...line, infinity, infinity]),
+        Array(image[0].length + 4).fill(infinity),
+        Array(image[0].length + 4).fill(infinity),
+    ];
+
+    const result = [
+        Array(image[0].length + 2).fill("."),
+        ...image.map(line => Array(line.length + 2).fill(".")),
+        Array(image[0].length + 2).fill("."),
+    ];
+
+    for (let rindex = 1; rindex < expanded.length - 1; rindex++) {
+        for (let cindex = 1; cindex < expanded[rindex].length - 1; cindex++) {
+            let lookup = 0;
+            if (expanded[rindex - 1][cindex - 1] === "#" ) {
+                lookup += 256;
+            }
+            if (expanded[rindex - 1][cindex] === "#" ) {
+                lookup += 128;
+            }
+            if (expanded[rindex - 1][cindex + 1] === "#" ) {
+                lookup += 64;
+            }
+            if (expanded[rindex][cindex - 1] === "#" ) {
+                lookup += 32;
+            }
+            if (expanded[rindex][cindex] === "#" ) {
+                lookup += 16;
+            }
+            if (expanded[rindex][cindex + 1] === "#" ) {
+                lookup += 8;
+            }
+            if (expanded[rindex + 1][cindex - 1] === "#" ) {
+                lookup += 4;
+            }
+            if (expanded[rindex + 1][cindex] === "#" ) {
+                lookup += 2;
+            }
+            if (expanded[rindex + 1][cindex + 1] === "#" ) {
+                lookup += 1;
+            }
+            result[rindex - 1][cindex - 1] = map[lookup];
+        }
+    }
+
+    return result;
+}
+
+const partOne = (input: Input, debug: boolean) => {
     if (debug) {
         console.log("-------Debug-----");
     }
 
-    return 0;
+    const step1 = stepImage(input.map, input.image, 0);
+    const step2 = stepImage(input.map, step1, 1);
+
+    const litPixels = step2.reduce((acc, line) => acc + line.filter(pixel => pixel === "#").length, 0);
+
+    return litPixels;
 };
 
-const partTwo = (input: number[], debug: boolean) => {
+const getInfityStone = (map: Map, step: number): Pixel => {
+    const start = map[0];
+    const end = map[map.length - 1];
+    if (start === ".") {
+        return "."
+    }
+    if (end === "#") {
+        return step === 0 ? "." : "#";
+    }
+    return (step % 2 === 0) ? "." : "#";
+}
+
+const partTwo = (input: Input, debug: boolean) => {
     if (debug) {
         console.log("-------Debug-----");
     }
 
-    return 0;
+    const limit = 50;
+    let image = input.image;
+
+    for (let step = 0; step < limit; step++) {
+        image = stepImage(input.map, image, step);
+    }
+
+    const litPixels = image.reduce((acc, line) => acc + line.filter(pixel => pixel === "#").length, 0);
+
+    return litPixels;
 };
 
 const resultOne = (_: any, result: number) => {
@@ -31,15 +123,19 @@ const resultTwo = (_: any, result: number) => {
 };
 
 
-const showInput = (input: number[]) => {
+const showInput = (input: Input) => {
     console.log(input);
 };
 
-const test = (_: number[]) => {
-    console.log("----Test-----");
+const test = (_: Input) => {
+    let result = "";
+    for (let index = 0; index < 20; index++) {
+        result += getInfityStone(["#", "#", "."], index);
+    }
+    console.log(result);
 };
 
-export const solutionTwenty: Puzzle<number[], number> = {
+export const solutionTwenty: Puzzle<Input, number> = {
     day: 20,
     input: processInput,
     partOne,
@@ -49,3 +145,5 @@ export const solutionTwenty: Puzzle<number[], number> = {
     showInput,
     test,
 }
+
+
