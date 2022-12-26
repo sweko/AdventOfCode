371ef54edd3486688a3e42ab4d8b6757ea4f7e18
Wekoslav Stefanovski <swekster@gmail.com>
2022-12-26 08:39:22 +0100

    2022 - Day 25 solution

--- 2022/code/day-25.ts
+++ 2022/code/day-25.ts
@@ -7,15 +7,59 @@ const processInput = async (day: number) => {
     return input;
 };
 
+const digitMap: Record<string, number> = {
+    "0": 0,
+    "1": 1,
+    "2": 2,
+    "-": -1,
+    "=": -2,
+}
+
+
+const pentaryToDecimal = (input: string):number => input
+    .split("")
+    .map(digit => digitMap[digit])
+    .reverse()
+    .map((digit, index) => 5 ** index * digit)
+    .sum();
+
+
+const decimalToPentary = (value: number):string => {
+    if (value === 0) return "0";
+
+    const result = [];
+    while (value !== 0) {
+        const digit = value % 5;
+        if ([0, 1, 2].includes(digit)) {
+            value = (value - digit) / 5;
+            result.unshift(digit.toString());
+            continue;
+        } else if (digit === 3) {
+            value = (value + 2) / 5;
+            result.unshift("=");
+            continue;
+        } else if (digit === 4) {
+            value = (value + 1) / 5;
+            result.unshift("-");
+            continue;
+        }
+        throw new Error(`Invalid digit ${digit} for value ${value}`);
+    }
+
+    return result.join("");
+};
+
 const partOne = (input: string[], debug: boolean) => {
     if (debug) {
         console.log("-------Debug-----");
     }
 
-    return 0;
+    const total = input.map(value => pentaryToDecimal(value)).sum();
+
+    return decimalToPentary(total);
 };
 
-const resultOne = (_: string[], result: number) => {
+const resultOne = (_: string[], result: string) => {
     return `Result part one is ${result}`;
 };
 
@@ -28,7 +72,7 @@ const test = (_: string[]) => {
     console.log("----Test-----");
 };
 
-export const solutionTwentyFive: Puzzle<string[], number> = {
+export const solutionTwentyFive: Puzzle<string[], string> = {
     day: 25,
     input: processInput,
     partOne,
