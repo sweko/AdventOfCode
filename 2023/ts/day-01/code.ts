b107da86042c1d87b7488fa230fd5898fd198692
Wekoslav Stefanovski <swekster@gmail.com>
2023-12-02 18:58:20 +0100

    2023, Day 1, solved

--- 2023/ts/day-01/code.ts
+++ 2023/ts/day-01/code.ts
@@ -9,45 +9,80 @@ const processInput = (day: number) => {
     return input;
 };
 
-const mapFirstLast = (input: string[]) => {
+// this is terribly inneficient, but it works
+const getFirstDigit = (input: string, map: Record<string, number>) => {
+    for (let index = 0; index < input.length; index++) {
+        const part = input.substring(index, input.length);
+        for (const key in map) {
+            if (part.startsWith(key)) {
+                return map[key];
+            }
+        }
+    }
+    return Number.NEGATIVE_INFINITY;
+};
+
+const getLastDigit = (input: string, map: Record<string, number>) => {
+    for (let index = input.length; index > 0; index--) {
+        const part = input.substring(0, index);
+        for (const key in map) {
+            if (part.endsWith(key)) {
+                return map[key];
+            }
+        }
+    }
+    return Number.NEGATIVE_INFINITY;
+}
+
+const mapFirstLast = (input: string[], map: Record<string, number>) => {
     return input.map(line => {
-        const chars = line.split("");
-        const first = chars.find(char => char.match(/[0-9]/))!;
-        const last = chars.findLast!(char => char.match(/[0-9]/))!;
-        return {
-            first: +first,
-            last: +last
-        };
+        const first = getFirstDigit(line, map);
+        const last = getLastDigit(line, map);
+        return { first, last };
     });
 }
 
 const partOne = (input: string[], debug: boolean) => {
-    const fls = mapFirstLast(input);
-    const result = fls.sum(fl => fl.first*10 + fl.last);
+    const map: Record<string, number> = {
+        "1": 1,
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+    }
+    const fls = mapFirstLast(input, map);
+    const result = fls.sum(fl => fl.first * 10 + fl.last);
     return result;
 };
 
 const partTwo = (input: string[], debug: boolean) => {
-    const map: Record<string, string> = {
-        "one": "1",
-        "two": "2",
-        "three": "3",
-        "four": "4",
-        "five": "5",
-        "six": "6",
-        "seven": "7",
-        "eight": "8",
-        "nine": "9",
+    const map: Record<string, number> = {
+        "1": 1,
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9
     }
-    const numNames =  Object.keys(map);
-    const numRegex = new RegExp(numNames.join("|"), "g");
-    const modInput: string[] = [];
-    for (const string of input) {
-        const result = string.replaceAll(numRegex, (match) => map[match]);
-        modInput.push(result);
-    }
-    const fls = mapFirstLast(modInput);
-    return fls.sum(fl => fl.first*10 + fl.last);
+    const fls = mapFirstLast(input, map);
+    const result = fls.sum(fl => fl.first * 10 + fl.last);
+    return result;
 };
 
 const resultOne = (_: any, result: number) => {
