60786aa0cdf406b4085c8b731cd0f0676b69e278
Wekoslav Stefanovski <swekster@gmail.com>
2023-12-06 09:06:13 +0100

    2023, Day 6, solved

---
+++ 2023/ts/day-06/code.ts
@@ -0,0 +1,87 @@
+// Solution for day 6 of advent of code 2023
+
+import { readInputLines, readInput } from "../system/aoc-helper";
+import "../utils/array-helpers";
+import { Puzzle } from "../model/puzzle";
+
+interface Race {
+    time: number;
+    record: number
+}
+
+
+const processInput = (day: number) => {
+    const [times, records] = readInputLines(day);
+    const tmatch = times.match(/^Time:\s+(.*)$/);
+    const rmatch = records.match(/^Distance:\s+(.*)$/);
+
+    if (!tmatch || !rmatch) {
+        throw Error("Invalid input");
+    }
+    const tvalues = tmatch[1].split(" ").filter(w => w).map(n => parseInt(n));
+    const rvalues = rmatch[1].split(" ").filter(w => w).map(n => parseInt(n));
+
+    const result = [] as Race[];
+    for (let index = 0; index < tvalues.length; index++) {
+        const time = tvalues[index];
+        const record = rvalues[index];
+        result.push({ time, record });
+    }
+    return result;
+};
+
+const getDistances = (time:number) => {
+    const result = [];
+    for (let index = 0; index <= time; index++) {
+        const distance = index * (time - index);
+        result.push(distance);
+    }
+    return result;
+}
+
+const partOne = (input: Race[], debug: boolean) => {
+    let total = 1;
+    for (const {time, record} of input) {
+        const distances = getDistances(time);
+        const bigger = distances.filter(d => d > record);
+        total *= bigger.length;
+    }
+    return total;
+};
+
+const partTwo = (input: Race[], debug: boolean) => {
+    const actualTime = +input.map(r => r.time).join("");
+    const actualRecord = +input.map(r => r.record).join("");
+
+    const distances = getDistances(actualTime);
+    const bigger = distances.filter(d => d > actualRecord);
+
+    return bigger.length;
+};
+
+const resultOne = (_: any, result: number) => {
+    return `Result part one is ${result}`;
+};
+
+const resultTwo = (_: any, result: number) => {
+    return `Result part two is ${result}`;
+};
+
+const showInput = (input: Race[]) => {
+    console.log(input);
+};
+
+const test = (_: Race[]) => {
+    console.log("----Test-----");
+};
+
+export const solution: Puzzle<Race[], number> = {
+    day: 6,
+    input: () => processInput(6),
+    partOne,
+    partTwo,
+    resultOne: resultOne,
+    resultTwo: resultTwo,
+    showInput,
+    test,
+}
