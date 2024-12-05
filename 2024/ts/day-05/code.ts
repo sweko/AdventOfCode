fcde2fc015e0ac58da9efbdc05f21c2ecf112041
Wekoslav Stefanovski <swekster@gmail.com>
2024-12-05 17:12:54 +0100

    AoC 2024 - Day 05 (full solution, unoptimized)

---
+++ 2024/ts/day-05/code.ts
@@ -0,0 +1,135 @@
+// Solution for day 5 of advent of code 2024
+
+import { readInputLines, readInput } from "../system/aoc-helper";
+import "../utils/array-helpers";
+import { Puzzle } from "../model/puzzle";
+import { parse } from "path";
+
+type Input = {
+    ruleset: [number, number][],
+    updates: number[][];
+}
+
+const processInput = (day: number) => {
+    const lines = readInputLines(day);
+    const result: Input = {
+        ruleset: [],
+        updates: [],
+    }
+
+    let currentLine = lines.shift();
+    while (!!currentLine) {
+        // get the rulesets
+        const ruleset = currentLine.split("|").map(value => parseInt(value, 10)) as [number, number];
+        result.ruleset.push(ruleset);
+        currentLine = lines.shift();
+    }
+
+    //get the updates
+    currentLine = lines.shift();
+    while (!!currentLine) {
+        const update = currentLine.split(",").map(value => parseInt(value, 10));
+        result.updates.push(update);
+        currentLine = lines.shift();
+    }
+
+    return result;
+};
+
+// this can be optimized
+const checkUpdate = (update: number[], ruleset: [number, number][]) => {
+    for (let findex = 0; findex < update.length; findex++) {
+        const first = update[findex];
+        for (let sindex = findex + 1; sindex < update.length; sindex++) {
+            const second = update[sindex];
+            if (ruleset.some(([frule, srule]) => (frule === second && srule === first))) {
+                //console.log(`Failed update ${update} for ${first} and ${second}`);
+                return false;
+            }
+        }
+    }
+    //console.log(`Passed update ${update}`);
+    return true;
+};
+
+const partOne = (input: Input, debug: boolean) => {
+    let result = 0;
+    for (const update of input.updates) {
+        if (checkUpdate(update, input.ruleset)) {
+            // assumes that the update has an odd number of elements
+            result += update[(update.length - 1) / 2];
+        }
+    }
+    return result;
+};
+
+const findProblemIndices = (update: number[], ruleset: [number, number][]) => {
+    for (let findex = 0; findex < update.length; findex++) {
+        const first = update[findex];
+        for (let sindex = findex + 1; sindex < update.length; sindex++) {
+            const second = update[sindex];
+            if (ruleset.some(([frule, srule]) => (frule === second && srule === first))) {
+                //console.log(`Failed update ${update} for ${first} and ${second}`);
+                return [findex, sindex];
+            }
+        }
+    }
+    return null;
+}
+
+const fixUpdate = (update: number[], ruleset: [number, number][]) => {
+    const result = [...update];
+    let problemIndices = findProblemIndices(result, ruleset);
+    while (problemIndices) {
+        const [findex, sindex] = problemIndices;
+
+        const first = result[findex];
+        const second = result[sindex];
+
+        result[findex] = second;
+        result[sindex] = first;
+
+        problemIndices = findProblemIndices(result, ruleset);
+    }
+
+    return result;
+}
+
+const partTwo = (input: Input, debug: boolean) => {
+    let result = 0;
+    const invalidUpdates = input.updates.filter(update => !checkUpdate(update, input.ruleset));
+    for (const update of invalidUpdates) {
+        const fixed = fixUpdate(update, input.ruleset);
+        // assumes that the update has an odd number of elements
+        // console.log(`Fixed update ${update} to ${fixed}`);
+        result += fixed[(fixed.length - 1) / 2];
+    }
+    return result;
+};
+
+const resultOne = (_: any, result: number) => {
+    return `Result part one is ${result}`;
+};
+
+const resultTwo = (_: any, result: number) => {
+    return `Result part two is ${result}`;
+};
+
+const showInput = (input: Input) => {
+    console.log(input);
+};
+
+const test = (_: Input) => {
+    console.log("----Test-----");
+};
+
+export const solution: Puzzle<Input, number> = {
+    day: 5,
+    input: () => processInput(5),
+    partOne,
+    partTwo,
+    resultOne: resultOne,
+    resultTwo: resultTwo,
+    showInput,
+    test,
+}
